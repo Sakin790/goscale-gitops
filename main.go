b1f119b65fdde705926b1c11da9e975addc2b513
Sakin790 <sakinislam79@gmail.com>
2026-06-22 09:19:06 +0600

    Day 3 complete

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"gitops/database"
 	"gitops/handlers"
@@ -15,11 +16,17 @@ func main() {
 
 	database.StartBufferLogWorker()
 
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	serverAddr := ":" + port
+
 	http.HandleFunc("/status", handlers.StatusHandler)
 	http.HandleFunc("/", handlers.Root)
 
-	fmt.Println("🚀 High-Performance Buffer Engine initiated! Server running on http://localhost:8080")
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	fmt.Printf("🚀🌐 High-Performance Buffer Engine initiated! Server running on http://localhost%s\n", serverAddr)
+	if err := http.ListenAndServe(serverAddr, nil); err != nil {
 		log.Fatalf("Server failed to start: %v\n", err)
 	}
 }
